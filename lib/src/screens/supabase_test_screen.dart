eba7fd84712836f9014259ade847c8b5531cb000
Anna <anna.lazarenkova@gmail.com>
2023-05-04 16:03:14 +0300

    forward message to DB

--- lib/src/screens/supabase_test_screen.dart
+++ lib/src/screens/supabase_test_screen.dart
@@ -25,40 +25,59 @@ class _FireBaseTestScreenState extends State<SupabaseTestScreen> {
       appBar: AppBar(
         title: const Text("SUPABASE_TEST_SCREEN"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.all(60),
-            color: Colors.grey,
-            child: TextField(controller: database_controller),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("send to supabase database"),
-              IconButton(
-                  onPressed: () {
-                    final name = database_controller.text;
-                  },
-                  icon: const Icon(Icons.send)),
-            ],
-          ),
-          StreamBuilder(
-              stream: service.createStream(),
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return Center(child: CircularProgressIndicator());
-                  // case ConnectionState.done: // STREAM IS NEVER DONE
-                  default:
-                    if (snapshot.hasData) return Text(snapshot.data.toString());
-                    if (snapshot.hasError) return Text('${snapshot.error}');
-                    return Text("DEFAULT");
-                }
-              })
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(60),
+              color: Colors.grey,
+              child: TextField(controller: database_controller),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("send to supabase database"),
+                IconButton(
+                    onPressed: () async {
+                      printGreen('OLEN SIIN');
+                      final message = database_controller.text;
+                      try {
+                        await Supabase.instance.client
+                            .from('test')
+                            .upsert({'messages': message, 'author_id': 3});
+                        if (mounted) {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                            content: Text('Saved '),
+                          ));
+                        }
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text('Error saving '),
+                          backgroundColor: Colors.red,
+                        ));
+                      }
+                    },
+                    icon: const Icon(Icons.send)),
+              ],
+            ),
+            StreamBuilder(
+                stream: service.createStream(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                    // case ConnectionState.done: // STREAM IS NEVER DONE
+                    default:
+                      if (snapshot.hasData) return Text(snapshot.data.toString());
+                      if (snapshot.hasError) return Text('${snapshot.error}');
+                      return Text("DEFAULT");
+                  }
+                })
+          ],
+        ),
       ),
     );
   }
@@ -69,16 +88,18 @@ class DatabaseService {
 
   DatabaseService(this.client);
 
-  void listenToChatMessages() {
-    client.channel('public:test').on(
-      RealtimeListenTypes.postgresChanges,
-      ChannelFilter(event: '*', schema: '*'),
-      (payload, [ref]) {
-        // Handle realtime payload
-        printGreen(payload);
-        printGreen(ref);
-      },
-    ).subscribe();
+  void listenToChatMessages(String message, int userId) async {
+
+  // client.channel('public:test').on(
+  //   RealtimeListenTypes.postgresChanges,
+  //   ChannelFilter(event: '*', schema: '*'),
+  //   (payload, [ref]) {
+  //     // Handle realtime payload
+  //     printGreen(payload);
+  //     printGreen(ref);
+  //     printGreen('OLEN SIIN 333');
+  //   },
+  // ).subscribe();
   }
 
   Stream createStream() {
