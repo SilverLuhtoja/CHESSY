7c457eda88e959850a7170a025e74458ad6c2b6d
Anna <anna.lazarenkova@gmail.com>
2023-05-24 15:29:56 +0300

    history added and rules changed

--- lib/src/screens/how_to_play_screen.dart
+++ lib/src/screens/how_to_play_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../widgets/main_menu_widgets/buttons/button.dart';
 
@@ -10,149 +11,33 @@ class HowToPlayScreen extends StatefulWidget {
 }
 
 class _HowToPlayScreenState extends State<HowToPlayScreen> {
-  String rules = 'PAWNS are unusual because they move and capture in different ways: they move forward but capture diagonally.' +
-                            'Pawns can only move forward one square at a time, except for their very first move where they can move forward two squares.\n\n' +
-                            'Pawns have another special ability and that is that if a pawn reaches the other side of the board it can become any other chess piece' +
-                            '(called promotion) excluding a king.' +
-                            'A common misconception is that pawns may only be exchanged for a piece that has been captured. That is NOT true. A pawn is usually promoted to a queen. Only pawns may be promoted.';
-  String imageName = 'assets/pawn_move.jpg';
+  bool _isVisible = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("How To Play?"),
+        title: _isVisible ? const Text("History") : const Text('Rules'),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
           child: Column(
             children: [
-              SingleChildScrollView(
-                child: Row(children: [
-                  SelectionButton(
-                    text: 'PAWN',
-                    handler: () => setState(
-                      () {
-                        rules = 'PAWNS are unusual because they move and capture in different ways: they move forward but capture diagonally.' +
-                            'Pawns can only move forward one square at a time, except for their very first move where they can move forward two squares.\n\n' +
-                            'Pawns have another special ability and that is that if a pawn reaches the other side of the board it can become any other chess piece' +
-                            '(called promotion) excluding a king.' +
-                            'A common misconception is that pawns may only be exchanged for a piece that has been captured. That is NOT true. A pawn is usually promoted to a queen. Only pawns may be promoted.';
-                        imageName = 'assets/pawn_move.jpg';
-                      },
-                    ),
-                    special: false,
-                  ),
-                  const SizedBox(width: 5),
-                  SelectionButton(
-                    text: 'KING',
-                    handler: () => setState(
-                      () {
-                        rules =
-                            'The KING can only move ONE square in any direction - up, down, to the sides, and diagonally.';
-                        imageName = 'assets/king_move.jpg';
-                      },
-                    ),
-                    special: false,
-                  ),
-                  const SizedBox(width: 5),
-                  SelectionButton(
-                    text: 'QUEEN',
-                    handler: () => setState(
-                      () {
-                        rules =
-                            'The QUEEN can move in any one straight direction - forward, backward, sideways, or diagonally - as far as possible as long as she does not move through any of her own pieces.';
-                        imageName = 'assets/queen_move.png';
-                      },
-                    ),
-                    special: false,
-                  ),
-                  const SizedBox(width: 5),
-                  SelectionButton(
-                    text: 'En Passant',
-                    handler: () => setState(
-                      () {
-                        rules =
-                            'If a pawn moves out two squares on its first move, and by doing so lands to the side of an opponent´s pawn (effectively jumping past the other pawn´s ability to capture it), that other pawn has the option of capturing the first pawn as it passes by.\nThis special move must be done immediately after the first pawn has moved past, otherwise the option to capture it is no longer available.';
-                        imageName = 'assets/en_passant.png';
-                      },
-                    ),
-                    special: true,
-                  ),
-                ]),
-              ),
-              Row(children: [
-                SelectionButton(
-                  text: 'BISHOP',
-                  handler: () => setState(
-                    () {
-                      rules =
-                          'The BISHOP may move as far as it wants, but only diagonally. Each bishop starts on one color (light or dark) and must always stay on that color.';
-                      imageName = 'assets/bishop_move.png';
-                    },
-                  ),
-                  special: false,
-                ),
-                const SizedBox(width: 5),
-                SelectionButton(
-                  text: 'KNIGHT',
-                  handler: () => setState(
-                    () {
-                      rules =
-                          'The KNIGHT move in a very different way from the other pieces – going two squares in one direction, and then one more move at a 90-degree angle, just like the shape of an “L”. \nKnights are also the only pieces that can move over other pieces.';
-                      imageName = 'assets/knight_move.jpg';
-                    },
-                  ),
-                  special: false,
-                ),
-                const SizedBox(width: 5),
-                SelectionButton(
-                  text: 'ROOK',
-                  handler: () => setState(
-                    () {
-                      rules =
-                          'The ROOK may move as far as it wants, but only forward, backward, and to the sides.';
-                      imageName = 'assets/rook_move.png';
-                    },
-                  ),
-                  special: false,
-                ),
-                const SizedBox(width: 5),
-                SelectionButton(
-                  text: 'CASTLING',
-                  handler: () => setState(
-                    () {
-                      rules =
-                          'Special rule called castling. This move allows you to do two important things all in one move: get your king'
-                          +' to safety (hopefully), and get your rook out of the corner and into the game.\nOn a player´s turn he may move '
-                          + 'his king two squares over to one side and then move the rook from that side´s corner to right next to the king on'
-                          +' the opposite side. However, in order to castle, the following conditions must be met:'
-                          +'\n* it must be that king`s very first move \n* it must be that rook´s very first move \n* there cannot be any pieces between '
-                          + 'the king and rook \n* to move the king may not be in check or pass through check';
-                      imageName = 'assets/castling.gif';
-                    },
-                  ),
-                  special: true,
-                ),
-              ]),
-              const SizedBox(
-                height: 20,
-              ),
-              Image.asset(
-                imageName,
-                height: 200,
-              ),
-              const SizedBox(
-                height: 20,
+              Visibility(
+                visible: _isVisible,
+                child: History(),
               ),
               Visibility(
-                visible: true,
-                maintainSize: true, //NEW
-                maintainAnimation: true, //NEW
-                maintainState: true, //NEW
-                child: Text(rules),
+                visible: !_isVisible,
+                child: const Rules(),
               ),
+              const SizedBox(height: 30),
+              MenuButton(
+                  text: _isVisible ? 'RULES' : 'BACK TO HISTORY',
+                  handler: () => setState(() {
+                        _isVisible = !_isVisible;
+                      })),
             ],
           ),
         ),
@@ -161,16 +46,218 @@ class _HowToPlayScreenState extends State<HowToPlayScreen> {
   }
 }
 
-class SelectionButton extends StatelessWidget {
-  final VoidCallback? handler;
-  final String text;
-  final bool special;
+class History extends StatelessWidget {
+  History({super.key});
 
-  const SelectionButton({super.key, required this.text, required this.handler, required this.special});
+  String chessHistory =
+      'The game of chess is believed to have originated in India, where it was call Chaturange prior to the 6th century AD. The game became popular in India and then spread to Persia, and the Arabs. The Arabs coined the term “Shah Mat”, which translates to “the King is dead”. This is where the word “checkmate” came from.';
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        width: 85, child: FilledButton(onPressed: handler, child: Text(text, style: TextStyle(  fontStyle: special? FontStyle.italic : FontStyle.normal))),);
+    return Column(children: [
+      SizedBox(height: 20),
+      Image.asset(
+        'assets/chahMat.jpg',
+        height: 200,
+      ),
+      SizedBox(height: 30),
+      Text(chessHistory),
+      SizedBox(height: 30),
+      Image.asset(
+        'assets/chess-pieces.jpg',
+        height: 100,
+      ),
+    ]);
+  }
+}
+
+class Rules extends StatefulWidget {
+  const Rules({super.key});
+
+  @override
+  State<Rules> createState() => _RulesState();
+}
+
+class _RulesState extends State<Rules> {
+  String _imageName = 'assets/pawn_move.jpg';
+  String _selected = 'pawn';
+
+  var gameRules = {
+    "pawn": {
+      "image": "assets/pawn_move.jpg",
+      'text':
+          'PAWNS are unusual because they move and capture in different ways\n',
+      "MOVE":
+          "MOVE:\n* If first move: can move forward one or two squares.\n* All other moves: forward one square at a time\n\n ATTACK: diagonally",
+      'SPECIAL': 'En Passant'
+    },
+    "knight": {
+      "image": "assets/knight_move.jpg",
+      'text': 'KNIGHTS are the only pieces that can move over other pieces.\n',
+      "MOVE":
+          "MOVE and ATTACK:\ngoing two squares in one direction + then one more move at a 90-degree angle.\n\n * just like the shape of an “L”",
+    },
+    "king": {
+      "image": "assets/king_move.jpg",
+      "MOVE":
+          "MOVE and ATTACK:\n\n * ONE square in any direction:\n *up \n*down \n *sideways\n *diagonally",
+    },
+    "queen": {
+      "image": "assets/queen_move.png",
+      "MOVE": "MOVE and ATTACK:\n\n *up \n*down \n *sideways\n *diagonally",
+    },
+    "bishop": {
+      "image": "assets/bishop_move.png",
+      "MOVE": "MOVE and ATTACK:\n\n *only diagonally",
+    },
+    "rook": {
+      "image": "assets/rook_move.png",
+      "MOVE": "MOVE and ATTACK:\n\n *up \n*down \n *sideways",
+      'SPECIAL': 'Castling'
+    },
+    "En Passant": {
+      'text': 'PAWN Special Rule: \n\n',
+      "image": "assets/en_passant.png",
+      "MOVE":
+          "If a pawn moves out two squares on its first move, and by doing so lands to the side of an opponent´s pawn (effectively jumping past the other pawn´s ability to capture it), that other pawn has the option of capturing the first pawn as it passes by.\nThis special move must be done immediately after the first pawn has moved past, otherwise the option to capture it is no longer available."
+    },
+    "Castling": {
+      'text': 'ROOK Special Rule: \n\n',
+      "image": "assets/en_passant.png",
+      "MOVE":
+          "On a player´s turn he may move his king two squares over to one side and then move the rook from that side´s corner to right next to the king on the opposite side. However, in order to castle, the following conditions must be met:\n* it must be that king`s very first move \n* it must be that rook´s very first move \n* there cannot be any pieces between the king and rook \n* to move the king may not be in check or pass through check"
+    }
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    double c_width = MediaQuery.of(context).size.width * 0.7;
+
+    return Column(children: [
+      Row(
+        children: [
+          SizedBox(
+              width: 30,
+              child: Column(children: [
+                SelectionButton(
+                    location: 'assets/PAWN.svg',
+                    handler: () => setState(() {
+                          _selected = 'pawn';
+                        }),
+                    color: _selected == 'pawn' ? Colors.blue : Colors.grey),
+                SizedBox(height: 5),
+                SelectionButton(
+                    location: 'assets/BISHOP.svg',
+                    handler: () => setState(() {
+                          _selected = 'bishop';
+                        }),
+                    color: _selected == 'bishop' ? Colors.blue : Colors.grey),
+                SizedBox(height: 5),
+                SelectionButton(
+                    location: 'assets/KING.svg',
+                    handler: () => setState(() {
+                          _selected = 'king';
+                        }),
+                    color: _selected == 'king' ? Colors.blue : Colors.grey),
+                SizedBox(height: 5),
+                SelectionButton(
+                    location: 'assets/QUEEN.svg',
+                    handler: () => setState(() {
+                          _selected = 'queen';
+                        }),
+                    color: _selected == 'queen' ? Colors.blue : Colors.grey),
+                SizedBox(height: 5),
+                SelectionButton(
+                    location: 'assets/KNIGHT.svg',
+                    handler: () => setState(() {
+                          _selected = 'knight';
+                        }),
+                    color: _selected == 'knight' ? Colors.blue : Colors.grey),
+                SizedBox(height: 5),
+                SelectionButton(
+                    location: 'assets/ROOK.svg',
+                    handler: () => setState(() {
+                          _selected = 'rook';
+                        }),
+                    color: _selected == 'rook' ? Colors.blue : Colors.grey),
+              ])),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Container(
+              width: c_width,
+              child: Column(
+                children: [
+                  //Title
+                  Text(_selected.toUpperCase(), style: const TextStyle(fontSize: 24),),
+                                    const SizedBox(height: 10),
+                  //Some extra info
+                  gameRules[_selected]!['text'] != null
+                      ? Text(gameRules[_selected]!['text'] as String)
+                      : Container(),
+                  //Image
+                  Image.asset(
+                    gameRules[_selected]!['image'] as String,
+                    height: 200,
+                  ),
+                  const SizedBox(height: 10),
+                  //Move and attack info
+                  Text(gameRules[_selected]!['MOVE'] as String),
+                  gameRules[_selected]!['SPECIAL'] != null
+                      ? Container(
+                          child: Row(
+                          children: [
+                            Text('SPECIAL RULE:'),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            ElevatedButton(
+                                onPressed: () => setState(() {
+                                      _selected =
+                                          gameRules[_selected]!['SPECIAL']
+                                              as String;
+                                    }),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black,
+                                ),
+                                child: Text(
+                                  gameRules[_selected]!['SPECIAL'] as String,
+                                ))
+                          ],
+                        ))
+                      : Container()
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    ]);
+  }
+}
+
+class SelectionButton extends StatelessWidget {
+  final VoidCallback? handler;
+  final String location;
+  final Color color;
+
+  const SelectionButton(
+      {super.key,
+      required this.location,
+      required this.handler,
+      required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        color: color,
+      ),
+      child: IconButton(
+        icon: SvgPicture.asset(location),
+        iconSize: 50,
+        onPressed: handler,
+      ),
+    );
   }
 }
